3c0f300177fc9627a4f0545fcb6cc20fd4c543f2
ltbar <ltbar@ip-10-51-55-74.eu-west-1.compute.internal>
2013-07-19 09:23:08 +0200

    reorganise filereorganise filess

---
+++ printFile.js
@@ -0,0 +1,18 @@
+/**
+ * Print a file's metadata.
+ *
+ * @param {String} fileId ID of the file to print metadata for.
+ */
+function printFile(fileId) {
+  var request = gapi.client.drive.files.get({
+    'fileId': fileId
+	});
+  request.execute(function(resp) {
+    console.log('Title: ' + resp.title);
+    console.log('Description: ' + resp.description);
+    console.log('MIME type: ' + resp.mimeType);
+    console.log('Download URL: ' + resp.downloadUrl);
+  });
+}
+
+
